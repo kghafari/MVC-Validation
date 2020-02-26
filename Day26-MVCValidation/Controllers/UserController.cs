454d835a3499eddad959e6f3166630049b23b0b7
kghafari <kghafari@users.noreply.github.com>
2020-02-25 21:42:01 -0500

    in-class example

---
+++ Day26-MVCValidation/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Day26_MVCValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day26_MVCValidation.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserForm()
+        {
+            return View(new User());
+        }
+
+        public IActionResult DisplayUser(User user)
+        {
+            if(ModelState.IsValid) //checks if the model object is valid(meets data annotation requirements)
+            { 
+            return View();
+            }
+            else
+            {
+                //the model object of type User did not match the requirements we made in our user class
+                return View("UserForm", user);
+            }
+        }
+    }
+}
---
+++ Day26-MVCValidation/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day26_MVCValidation.Models
+{
+    public class User
+    {
+        //data annotations go in [] brackets. Needs a `using System.ComponentModel.DataAnnotations` using statement.
+        //only applicable to the Property directly below it.
+        // types of annotations: 
+            //[Required]
+            //[StringLength(n, MinimumLength = j, ErrorMessage = "Your message."] - Forces a string length between n and j
+            //[EmailAddress] - does magic email address validation
+            //[RegularExpression("regex here")] - does regex validation
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Must be between 3 and 30 characters.")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$")]
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        [StringLength(2, MinimumLength = 2)]
+        public string State { get; set; }
+
+        [RegularExpression(@"\d{5}", ErrorMessage = "Zip Code must be 5 digits.")]
+        public string ZipCode { get; set; }
+        public string FavProWrestler { get; set; }
+
+        //make regex for sql server DATE
+        public DateTime DOB { get; set; }
+
+
+    }
+}
